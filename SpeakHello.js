ecbfdab07ad093c77c36e9631f1fdbb56cf1377a
Stevecmd <72811315+Stevecmd@users.noreply.github.com>
2020-10-31 10:44:33 +0300

    Indentation 2
    Still facing the same indentation error

--- SpeakHello.js
+++ SpeakHello.js
@@ -24,11 +24,11 @@
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
 (function(window) {
-	    var speakWord = "Hello";
-	    var helloSpeaker = function (name) {
-		          console.log(speakWord + " " + name);
-	    }
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
 
-	    window.helloSpeaker = helloSpeaker;
+	window.helloSpeaker = helloSpeaker;
 
 })(window);
